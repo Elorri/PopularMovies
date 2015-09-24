3c9964a85fbc9dc078df6365fb3b85185b66c344
Elorri <etchemendy.elorri@gmail.com>
2015-09-24 16:08:55 +0200

    Add network call to TheMovieDb

--- app/src/main/java/com/example/android/popularmovies/DiscoveryFragment.java
+++ app/src/main/java/com/example/android/popularmovies/DiscoveryFragment.java
@@ -1,13 +1,22 @@
 package com.example.android.popularmovies;
 
-import android.support.v4.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -38,9 +47,9 @@ public class DiscoveryFragment extends Fragment {
         List<String> imageNames = new ArrayList<String>(Arrays.asList(data));
 
         // The ArrayAdapter will take data and populate the GridView it's attached to.
-        mDiscoveryAdapter = new ArrayAdapter<String>( getActivity(),  R.layout.grid_item_layout, R.id.grid_item_layout, imageNames);
+        mDiscoveryAdapter = new ArrayAdapter<String>(getActivity(), R.layout.grid_item_layout, R.id.grid_item_layout, imageNames);
 
-        View rootView=inflater.inflate(R.layout.fragment_discovery, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_discovery, container, false);
 
 
         // Get a reference to the ListView, and attach this adapter to it.
@@ -48,4 +57,93 @@ public class DiscoveryFragment extends Fragment {
         gridView.setAdapter(mDiscoveryAdapter);
         return rootView;
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FetchMoviesTask movieTask = new FetchMoviesTask();
+        movieTask.execute();
+    }
+
+
+    public class FetchMoviesTask extends AsyncTask<Void, Void, Void> {
+
+        private final String LOG_TAG = FetchMoviesTask.class.getSimpleName();
+        private final String API_KEY = "4691965cfc3e6f0591bc595986e92e84";
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            URL url = constructTMDbURL(API_KEY);
+            Log.d("PopularMovies", url.toString());
+            String forecastJsonStr = getJsonString(url);
+
+            return null;
+        }
+
+        private URL constructTMDbURL(String key) {
+            try {
+                return new URL("http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" + key);
+            } catch (MalformedURLException e) {
+                Log.e(LOG_TAG, "Error " + e);
+                return null;
+            }
+        }
+
+
+        private String getJsonString(URL url) {
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String moviesJsonStr = null;
+
+            try {
+                // Create the request http, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                moviesJsonStr = buffer.toString();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+            return moviesJsonStr;
+        }
+    }
+
 }
