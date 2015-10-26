21421d86990a314b2979a4648e9e43dd3b51ddb5
Elorri <etchemendy.elorri@gmail.com>
2015-10-26 18:30:59 +0100

    Add tests for all tables in TestDb

--- app/src/androidTest/java/com/example/android/popularmovies/data/TestDb.java
+++ app/src/androidTest/java/com/example/android/popularmovies/data/TestDb.java
@@ -1,5 +1,6 @@
 package com.example.android.popularmovies.data;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.test.AndroidTestCase;
@@ -15,6 +16,12 @@ import java.util.HashSet;
  * Created by Elorri on 26/10/2015.
  */
 public class TestDb extends AndroidTestCase {
+
+
+    @Override
+    protected void setUp() throws Exception {
+        mContext.deleteDatabase(MoviesDbHelper.DATABASE_NAME);
+    }
 
     public void testCreateDb() throws Throwable {
         // Build a HashSet of all of the table names we wish to look for
@@ -48,6 +55,165 @@ public class TestDb extends AndroidTestCase {
 
         db.close();
     }
+
+    public void testMovieTable() {
+        // First step: Get reference to writable database
+        SQLiteDatabase db = new MoviesDbHelper(this.mContext).getWritableDatabase();
+        assertEquals(true, db.isOpen());
+
+        // Second Step: Create ContentValues of what you want to insert
+        ContentValues movieValues =  new ContentValues();
+        movieValues.put(MovieEntry._ID,135398);
+        movieValues.put(MovieEntry.COLUMN_TITLE,"Jurassic World");
+        movieValues.put(MovieEntry.COLUMN_DURATION,120);
+        movieValues.put(MovieEntry.COLUMN_RELEASE_DATE,189978989);
+        movieValues.put(MovieEntry.COLUMN_POSTER_PATH,"/jjBgi2r5cRt36xF6iNUEhzscEcb.jpg");
+        movieValues.put(MovieEntry.COLUMN_PLOT_SYNOPSIS,"Twenty-two years after the events of Jurassic Park, Isla Nublar now features a fully functioning dinosaur theme park, Jurassic World, as originally envisioned by John Hammond.");
+        movieValues.put(MovieEntry.COLUMN_RATE,6.9);
+        movieValues.put(MovieEntry.COLUMN_POPULARITY,"43.57727");
+        movieValues.put(MovieEntry.COLUMN_FAVORITE, 1);
+
+        // Third Step: Insert ContentValues into database and get a row ID back
+        long rowId = db.insert(MovieEntry.TABLE_NAME, null, movieValues);
+
+        // Verify we got a row back.
+        assertTrue(rowId != -1);
+
+        // Fourth Step: Query the database and receive a Cursor back
+        Cursor cursor = db.query(
+                MovieEntry.TABLE_NAME,  // Table to Query
+                null, // all columns
+                null, // Columns for the "where" clause
+                null, // Values for the "where" clause
+                null, // columns to group by
+                null, // columns to filter by row groups
+                null // sort order
+        );
+
+        // Move the cursor to a valid database row and check to see if we got any records back
+        // from the query
+        assertTrue( "Error: No Records returned from query", cursor.moveToFirst() );
+
+        // Fifth Step: Validate data in resulting Cursor with the original ContentValues
+        // (you can use the validateCurrentRecord function in TestUtilities to validate the
+        // query if you like)
+        TestUtilities.validateCurrentRecord("Error: Query Validation Failed",
+                cursor, movieValues);
+
+        // Move the cursor to demonstrate that there is only one record in the database
+        assertFalse( "Error: More than one record returned from query", cursor.moveToNext() );
+
+        // Sixth Step: Close Cursor and Database
+        cursor.close();
+        db.close();
+    }
+
+    public void testTrailerTable() {
+        testMovieTable(); //this test will add a movie in the db
+
+        // First step: Get reference to writable database
+        SQLiteDatabase db = new MoviesDbHelper(this.mContext).getWritableDatabase();
+        assertEquals(true, db.isOpen());
+
+
+        // Second Step: Create ContentValues of what you want to insert
+        ContentValues trailerValues =  new ContentValues();
+        trailerValues.put(TrailerEntry._ID,"559198cac3a3685710000b58");
+        trailerValues.put(TrailerEntry.COLUMN_KEY,"FRDdRto_3SA");
+        trailerValues.put(TrailerEntry.COLUMN_NAME,"Trailers From Hell");
+        trailerValues.put(TrailerEntry.COLUMN_TYPE,"Featurette");
+        trailerValues.put(TrailerEntry.COLUMN_MOVIE_ID,135398);
+
+
+        // Third Step: Insert ContentValues into database and get a row ID back
+        long rowId = db.insert(TrailerEntry.TABLE_NAME, null, trailerValues);
+
+        // Verify we got a row back.
+        assertTrue(rowId != -1);
+
+        // Fourth Step: Query the database and receive a Cursor back
+        Cursor cursor = db.query(
+                TrailerEntry.TABLE_NAME,  // Table to Query
+                null, // all columns
+                null, // Columns for the "where" clause
+                null, // Values for the "where" clause
+                null, // columns to group by
+                null, // columns to filter by row groups
+                null // sort order
+        );
+
+        // Move the cursor to a valid database row and check to see if we got any records back
+        // from the query
+        assertTrue( "Error: No Records returned from query", cursor.moveToFirst() );
+
+        // Fifth Step: Validate data in resulting Cursor with the original ContentValues
+        // (you can use the validateCurrentRecord function in TestUtilities to validate the
+        // query if you like)
+        TestUtilities.validateCurrentRecord("Error: Query Validation Failed",
+                cursor, trailerValues);
+
+        // Move the cursor to demonstrate that there is only one record in the database
+        assertFalse( "Error: More than one record returned from query", cursor.moveToNext() );
+
+        // Sixth Step: Close Cursor and Database
+        cursor.close();
+        db.close();
+    }
+
+
+    public void testReviewTable() {
+        testMovieTable(); //this test will add a movie in the db
+
+        // First step: Get reference to writable database
+        SQLiteDatabase db = new MoviesDbHelper(this.mContext).getWritableDatabase();
+        assertEquals(true, db.isOpen());
+
+        // Second Step: Create ContentValues of what you want to insert
+        ContentValues reviewValues =  new ContentValues();
+        reviewValues.put(ReviewEntry._ID,"55660928c3a3687ad7001db");
+        reviewValues.put(ReviewEntry.COLUMN_AUTHOR,"Phileas Fogg5");
+        reviewValues.put(ReviewEntry.COLUMN_CONTENT,"Fabulous action movie. Lots of interesting characters. They don''t make many movies like this. The whole movie from start to finish was entertaining I''m looking forward to seeing it again. I definitely recommend seeing it.");
+        reviewValues.put(ReviewEntry.COLUMN_MOVIE_ID,135398);
+
+
+
+        // Third Step: Insert ContentValues into database and get a row ID back
+        long rowId = db.insert(ReviewEntry.TABLE_NAME, null, reviewValues);
+
+        // Verify we got a row back.
+        assertTrue(rowId != -1);
+
+        // Fourth Step: Query the database and receive a Cursor back
+        Cursor cursor = db.query(
+                ReviewEntry.TABLE_NAME,  // Table to Query
+                null, // all columns
+                null, // Columns for the "where" clause
+                null, // Values for the "where" clause
+                null, // columns to group by
+                null, // columns to filter by row groups
+                null // sort order
+        );
+
+        // Move the cursor to a valid database row and check to see if we got any records back
+        // from the query
+        assertTrue( "Error: No Records returned from query", cursor.moveToFirst() );
+
+        // Fifth Step: Validate data in resulting Cursor with the original ContentValues
+        // (you can use the validateCurrentRecord function in TestUtilities to validate the
+        // query if you like)
+        TestUtilities.validateCurrentRecord("Error: Query Validation Failed",
+                cursor, reviewValues);
+
+        // Move the cursor to demonstrate that there is only one record in the database
+        assertFalse( "Error: More than one record returned from query", cursor.moveToNext() );
+
+        // Sixth Step: Close Cursor and Database
+        cursor.close();
+        db.close();
+    }
+
+
+
 
     private void checkColumnsCorrect(Cursor c, SQLiteDatabase db, String tableContractName) {
         if (tableContractName.equals(MovieEntry.class.getSimpleName())) {
@@ -126,6 +292,7 @@ public class TestDb extends AndroidTestCase {
             fail("Error");
         }
     }
+
 
 
 }
--- app/src/main/java/com/example/android/popularmovies/data/MoviesDbHelper.java
+++ app/src/main/java/com/example/android/popularmovies/data/MoviesDbHelper.java
@@ -25,7 +25,7 @@ public class MoviesDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        final String SQL_CREATE_MOVIE_TABLE="CREATE TABLE "+MovieEntry.TABLE_NAME+"("+MovieEntry._ID+" INTEGER NOT NULL, "+MovieEntry.COLUMN_TITLE+" TEXT NOT NULL,  "+MovieEntry.COLUMN_DURATION+" INTEGER,   "+MovieEntry.COLUMN_RELEASE_DATE+" INTEGER NOT NULL,   "+MovieEntry.COLUMN_POSTER_PATH+" TEXT NOT NULL ,  "+MovieEntry.COLUMN_PLOT_SYNOPSIS+" TEXT NOT NULL,   "+MovieEntry.COLUMN_RATE+" REAL NOT NULL ,  "+MovieEntry.COLUMN_POPULARITY+" REAL NOT NULL,   "+MovieEntry.COLUMN_FAVORITE+" INTEGER NOT NULL,   PRIMARY KEY( "+MovieEntry._ID+" ) ON CONFLICT REPLACE); " ;
+        final String SQL_CREATE_MOVIE_TABLE="CREATE TABLE "+MovieEntry.TABLE_NAME+"("+MovieEntry._ID+" INTEGER NOT NULL, "+MovieEntry.COLUMN_TITLE+" TEXT NOT NULL,  "+MovieEntry.COLUMN_DURATION+" INTEGER,   "+MovieEntry.COLUMN_RELEASE_DATE+" INTEGER NOT NULL,   "+MovieEntry.COLUMN_POSTER_PATH+" TEXT NOT NULL ,  "+MovieEntry.COLUMN_PLOT_SYNOPSIS+" TEXT NOT NULL,   "+MovieEntry.COLUMN_RATE+" REAL NOT NULL ,  "+MovieEntry.COLUMN_POPULARITY+" TEXT NOT NULL,   "+MovieEntry.COLUMN_FAVORITE+" INTEGER NOT NULL,   PRIMARY KEY( "+MovieEntry._ID+" ) ON CONFLICT REPLACE); " ;
         final String SQL_CREATE_TRAILER_TABLE="CREATE TABLE "+ReviewEntry.TABLE_NAME+" (   "+ReviewEntry._ID+" TEXT NOT NULL ,  "+ReviewEntry.COLUMN_AUTHOR+" TEXT NOT NULL ,  "+ReviewEntry.COLUMN_CONTENT+" TEXT NOT NULL,   "+ReviewEntry.COLUMN_MOVIE_ID+" INTEGER NOT NULL,   FOREIGN KEY( "+ReviewEntry.COLUMN_MOVIE_ID+" )   REFERENCES "+MovieEntry.TABLE_NAME+"( "+MovieEntry._ID+" ),   PRIMARY KEY( "+ReviewEntry._ID+" ) ON CONFLICT REPLACE); " ;
         final String SQL_CREATE_REVIEW_TABLE="CREATE TABLE "+TrailerEntry.TABLE_NAME+" (   "+TrailerEntry._ID+" TEXT NOT NULL ,  "+TrailerEntry.COLUMN_KEY+" TEXT NOT NULL UNIQUE ,  "+TrailerEntry.COLUMN_NAME+" TEXT NOT NULL ,  "+TrailerEntry.COLUMN_TYPE+" TEXT NOT NULL,   "+TrailerEntry.COLUMN_MOVIE_ID+" INTEGER NOT NULL,   FOREIGN KEY( "+TrailerEntry.COLUMN_MOVIE_ID+" )   REFERENCES "+MovieEntry.TABLE_NAME+"( "+MovieEntry._ID+" ),   PRIMARY KEY( "+TrailerEntry._ID+" ) ON CONFLICT REPLACE,  UNIQUE ( "+TrailerEntry.COLUMN_KEY+" ) ON CONFLICT REPLACE); "  ;
 
@@ -43,5 +43,14 @@ public class MoviesDbHelper extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + TrailerEntry.TABLE_NAME);
         db.execSQL("DROP TABLE IF EXISTS " + ReviewEntry.TABLE_NAME);
         onCreate(db);
+    }
+
+    @Override
+    public void onOpen(SQLiteDatabase db) {
+        super.onOpen(db);
+        if (!db.isReadOnly()) {
+            // Enable foreign key constraints
+            db.execSQL("PRAGMA foreign_keys=ON;");
+        }
     }
 }
