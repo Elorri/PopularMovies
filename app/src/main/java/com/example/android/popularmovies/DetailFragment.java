4410ad9139f11fcb9f703ed93e18ea6c3f1072b2
Elorri <etchemendy.elorri@gmail.com>
2015-10-26 09:34:36 +0100

    Add ShareActionProvider

--- app/src/main/java/com/example/android/popularmovies/DetailFragment.java
+++ app/src/main/java/com/example/android/popularmovies/DetailFragment.java
@@ -6,7 +6,13 @@ import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -19,6 +25,7 @@ import com.squareup.picasso.Picasso;
  */
 public class DetailFragment extends Fragment {
 
+    private static final String LOG_TAG = DetailFragment.class.getSimpleName();
     private Movie movie;
     private TmdbAccess tmdbAccess;
 
@@ -32,16 +39,39 @@ public class DetailFragment extends Fragment {
     private BroadcastReceiver receiver;
 
     public DetailFragment() {
+        setHasOptionsMenu(true);
     }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.fragment_detail, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        ShareActionProvider mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(createShareIntent());
+        } else {
+            Log.d(LOG_TAG, "Share Action Provider is null?");
+        }
+    }
+
+    private Intent createShareIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT,
+                "will be the first video URL here");
+        return shareIntent;
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
-        tmdbAccess=new TmdbAccess();
+        tmdbAccess = new TmdbAccess();
         Intent intent = getActivity().getIntent();
         if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-            movie =  intent.getParcelableExtra(Intent.EXTRA_TEXT);
+            movie = intent.getParcelableExtra(Intent.EXTRA_TEXT);
             title = (TextView) rootView.findViewById(R.id.title);
             posterImage = (ImageView) rootView.findViewById(R.id.posterImage);
             overview = (TextView) rootView.findViewById(R.id.overview);
@@ -51,7 +81,7 @@ public class DetailFragment extends Fragment {
 
             title.setText(movie.getTitle());
             overview.setText(movie.getOverview());
-            voteAverage.setText(movie.getVoteAverage()+getString(R.string.rateMax));
+            voteAverage.setText(movie.getVoteAverage() + getString(R.string.rateMax));
             releaseDate.setText(movie.getReleaseDate().split("-")[0]); //To extract the year from the date
             duration.setText(movie.getDuration() + " " + getString(R.string.min));
         }
