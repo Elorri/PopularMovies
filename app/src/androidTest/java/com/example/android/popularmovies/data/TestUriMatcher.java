a085951df833ddd8824539ef649c57560162718c
Elorri <etchemendy.elorri@gmail.com>
2015-10-27 18:02:09 +0100

    Bug; Urimatcher trials

--- app/src/androidTest/java/com/example/android/popularmovies/data/TestUriMatcher.java
+++ app/src/androidTest/java/com/example/android/popularmovies/data/TestUriMatcher.java
@@ -13,15 +13,16 @@ import com.example.android.popularmovies.data.MovieContract.TrailerEntry;
  */
 public class TestUriMatcher extends AndroidTestCase {
 
+
     public void testUriMatcher() {
         UriMatcher testMatcher = MovieProvider.buildUriMatcher();
         assertEquals("Error:",testMatcher.match(MovieEntry.CONTENT_URI), MovieProvider.MOVIE);
         assertEquals("Error:",testMatcher.match(MovieEntry.buildMovieSortByUri("popularity.desc")), MovieProvider.MOVIES_SORT_BY);
         assertEquals("Error:",testMatcher.match(MovieEntry.buildMovieFavoriteUri()), MovieProvider.MOVIES_FAVORITE);
-        assertEquals("Error:",testMatcher.match(MovieEntry.buildMovieDetailUri(135399l)), MovieProvider.MOVIE_DETAIL);
+        assertEquals("Error:",testMatcher.match(MovieEntry.buildMovieDetailUri(135399L)), MovieProvider.MOVIE_DETAIL);
         assertEquals("Error:",testMatcher.match(TrailerEntry.CONTENT_URI), MovieProvider.TRAILER);
-        assertEquals("Error:",testMatcher.match(TrailerEntry.buildMovieTrailerUri(135399l)), MovieProvider.TRAILERS_MOVIE);
+        assertEquals("Error:",testMatcher.match(TrailerEntry.buildMovieTrailerUri(135399L)), MovieProvider.TRAILERS_MOVIE);
         assertEquals("Error:",testMatcher.match(ReviewEntry.CONTENT_URI), MovieProvider.REVIEW);
-        assertEquals("Error:",testMatcher.match(ReviewEntry.buildMovieReviewUri(135399l)), MovieProvider.REVIEWS_MOVIE);
+        assertEquals("Error:",testMatcher.match(ReviewEntry.buildMovieReviewUri(135399L)), MovieProvider.REVIEWS_MOVIE);
     }
 }
